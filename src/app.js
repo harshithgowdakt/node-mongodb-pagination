98af919ee5956af76fd05629febf46ef4bd7cbf9
Harshith Gowda <harshith.gowda@intimetec.com>
2021-08-31 09:51:12 +0530

    pagination exmaple

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const mongoOp = require("./mongo")
 const { getDb } = require('./mongodb')
 const app = express()
 const router = express.Router()
+let db;
 
 router.get('/users', async (req, res) => {
     try {
@@ -10,13 +10,12 @@ router.get('/users', async (req, res) => {
         let size = parseInt(req.query.size)
         if (pageNo < 0 || pageNo === 0) {
             response = { "error": true, "message": "invalid page number, should start with 1" };
-            return res.json(response)
+            return res.json(JSON.stringify(response))
         }
         const skip = size * (pageNo - 1)
 
-        let db = await getDb();
         let totalCount = await db.collection('users').find({}).count();
-        let users = db.collection('users').find({}).limit(size).skip(skip);
+        let users = await db.collection('users').find({}).sort({ _id: 1 }).limit(size).skip(skip).toArray();
 
         let totalPages = Math.ceil(totalCount / size);
         res.json({ "error": false, "message": users, "pages": totalPages });
@@ -25,5 +24,13 @@ router.get('/users', async (req, res) => {
     }
 })
 
-app.use('/api', router)
-app.listen(3000)
+getDb()
+    .then((dbObject) => {
+        db = dbObject
+        app.use('/api', router)
+        app.listen(3000, () => console.log('server listening on port on 3000'));
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,10 +1,9 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-
-const dbName = 'pagination-pco';
 
 exports.getDb = async function () {
+    const url = 'mongodb://localhost:27017';
+    const client = new MongoClient(url);
+    const dbName = 'pagination-pco';
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
